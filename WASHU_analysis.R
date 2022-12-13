5a7ca9ed81143d56d6249b2466933616f342863c
Rege <rarege@luriechildrens.org>
2022-12-13 15:07:24 -0600

    added tests for tapse and one year paat rvet - RR

--- WASHU_analysis.R
+++ WASHU_analysis.R
@@ -39,10 +39,80 @@ paat_rvet_36 %>%
 
 paat_rvet_36_aov <- aov(PAAT_RVET_36 ~ `Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, data = paat_rvet_36)
 
-summary(paat_rvet_36_aov)  
-#wilberdingtrain
-#again
-#one more for the boys
+summary(paat_rvet_36_aov)
 
-## second one for the boys
+## PAAT RVET One
 
+paat_rvet_One <- WASHU_all_weeks %>%
+  select(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, PAAT_RVET_One) %>%
+  filter(!is.na(PAAT_RVET_One)) %>%
+  filter(!(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)` == 4))
+
+
+paat_rvet_One %>%
+  group_by(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`) %>%
+  summarize(mean = mean(PAAT_RVET_One))
+
+paat_rvet_One %>%
+  table()
+
+paat_rvet_One_aov <- aov(PAAT_RVET_One ~ `Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, data = paat_rvet_One)
+
+summary(paat_rvet_One_aov)  
+
+## TAPSE 32
+
+TAPSE_32 <- WASHU_all_weeks %>%
+  select(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, TAPSE_32) %>%
+  filter(!is.na(TAPSE_32)) %>%
+  filter(!(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)` == 4))
+
+
+TAPSE_32 %>%
+  group_by(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`) %>%
+  summarize(mean = mean(TAPSE_32))
+
+TAPSE_32 %>%
+  table()
+
+TAPSE_32_aov <- aov(TAPSE_32 ~ `Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, data = TAPSE_32)
+
+summary(TAPSE_32_aov)  
+
+## TAPSE 36
+
+TAPSE_36 <- WASHU_all_weeks %>%
+  select(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, TAPSE_36) %>%
+  filter(!is.na(TAPSE_36)) %>%
+  filter(!(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)` == 4))
+
+
+TAPSE_36 %>%
+  group_by(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`) %>%
+  summarize(mean = mean(TAPSE_36))
+
+TAPSE_36 %>%
+  table()
+
+TAPSE_36_aov <- aov(TAPSE_36 ~ `Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, data = TAPSE_36)
+
+summary(TAPSE_36_aov)
+
+## TAPSE One
+
+TAPSE_One <- WASHU_all_weeks %>%
+  select(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, TAPSE_One) %>%
+  filter(!is.na(TAPSE_One)) %>%
+  filter(!(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)` == 4))
+
+
+TAPSE_One %>%
+  group_by(`Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`) %>%
+  summarize(mean = mean(TAPSE_One))
+
+TAPSE_One %>%
+  table()
+
+TAPSE_One_aov <- aov(TAPSE_One ~ `Shenan (0=No BPD, 1= BPD, 4=dead, transfer, withdrew)`, data = TAPSE_One)
+
+summary(TAPSE_One_aov)
