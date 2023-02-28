8e4d5b1a0f32f0f6728b97d06d65f53df268decb
Raviraj Rege <ravirajrege2022@u.northwestern.edu>
2023-02-28 17:37:55 -0600

    updated R script

--- Genomic_All.R
+++ Genomic_All.R
@@ -1,4 +1,4 @@
 library(tidyverse)
-Raw_Annotation <- read_rds(file = "Wilberding_BPD_noannot_chr_hg38_hgmd.rds")
+Raw_Annotation <- read_rds(file = "Wilberding_BPD_noannot_chr_hg38_cardio.rds")
 Genotypes <- Raw_Annotation[["gt"]][["gt_GT"]]
 Allele_Frequency <- Raw_Annotation[["fix"]][["AF"]]
