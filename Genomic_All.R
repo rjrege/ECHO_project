a30e5fba2ef2f98c5a87ff1bc1b7ab0b379a7ff6
Raviraj Rege <ravirajrege2022@u.northwestern.edu>
2023-03-01 00:59:31 -0600

    downloaded the FREGAT package for FFBSKAT

--- Genomic_All.R
+++ Genomic_All.R
@@ -1,4 +1,12 @@
 library(tidyverse)
+library(FREGAT)
+
+WASHU_all_weeks <- read_csv(file = "WASHU_all_Weeks.csv")
 Raw_Annotation <- read_rds(file = "Wilberding_BPD_noannot_chr_hg38_cardio.rds")
+bed <- read.table("gene_list_50kbpad.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
+
 Genotypes <- Raw_Annotation[["gt"]][["gt_GT"]]
-Allele_Frequency <- Raw_Annotation[["fix"]][["AF"]]
+
+#test stuff, ignore
+data(example.data) 
+View(genodata)
