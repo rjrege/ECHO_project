c72f2574bcceb55a948f5307d012e56a85a9cf4a
Raviraj Rege <ravirajrege2022@u.northwestern.edu>
2023-03-08 23:24:16 -0600

    3-8 updates
    3-8 updates

--- Genomic_All.R
+++ Genomic_All.R
@@ -9,12 +9,28 @@ Genotypes <- Raw_Annotation[["gt"]]
 
 table(Genotypes$gt_GT)
 #dots might represent insertions/deletions? exclude for now
-dotted <- Genotypes %>%
-  filter(gt_GT == '0/.')
+keep_this <- Genotypes %>%
+  filter(gt_GT == '0/0' | gt_GT == '0/1' | gt_GT == '1/1') %>%
+  mutate(geno = getGene(gt_GT)) %>%
+  mutate(id = substr(Indiv, 1, 6)) %>%
+  select(id, geno, POS) %>%
+  group_by(POS) %>%
+  summarize(num = n())
 
-length(unique(dotted$POS))
-View(dotted)
-head(dotted)
+
 #test stuff, ignore
 data(example.data) 
 View(genodata)
+View(phenodata)
+View(kin)
+  getGene <- function(x){
+  
+  tranTable <- rbind( c("0/0", 0), c("0/1", 1), c("1/1", 2))
+  geno <- gsub(":.+", "", x)
+  for (i in 1:nrow(tranTable)){
+    
+    ind <- which(geno == tranTable[i,1])
+    geno[ind] <- as.numeric(tranTable[i,2])
+  }
+  return(geno)
+}
